e715db6c2479d93a2081985afc489be29880c1dd
Richard Kuang <boy12371@hotmail.com>
2017-04-09 13:15:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-$arr = array('测试', 'Hello PHP', '中国');
+$arr = array('word1'=>'测试', 'word2'=>'Hello PHP', 'word3'=>'中国');
 
-echo json_encode($arr);
+//echo json_encode($arr);
+foreach($arr as $key => $value){
+    $arr[$key] = urlencode($value);
+}
+
+echo urldecode(json_encode($arr));
 ?>
